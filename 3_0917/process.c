840cb6d5bf8daa2a228255833fc98209d5f756c5
wjy4ng <83898622+wjy4ng@users.noreply.github.com>
2025-09-22 13:47:21 +0900

    refact: 폴더 이름 변경

--- 3_0917/process.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main(){
-	printf("PID: %d\n", (int)getpid());
-	printf("PPID: %d\n", (int)getppid());
-	printf("PGID: %d\n", (int)getpgrp());
-	return 0;
-}
